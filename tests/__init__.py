9a903e88ba16331d92d1a4f28133ae55d6f94c01
SukramJ <sukramj@icloud.com>
2024-12-18 07:59:44 +0100

    Remove danielperna84 from links

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,5 +2,5 @@
 hahomematic is a Python 3 module for Home Assistant.
 
 It interacts with HomeMatic and HomematicIP devices.
-https://github.com/danielperna84/hahomematic.
+https://github.com/sukramj/hahomematic.
 """
